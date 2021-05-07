93bc62773471515e792fde0337e91e75db275c15
jiye <jiyecafe@gmail.com>
2021-05-07 21:26:02 +0800

    feat: 总过滤器。

---
+++ src/main/java/cn/bookmanage/filter/AllFilter.java
@@ -0,0 +1,28 @@
+package cn.bookmanage.filter; /**
+ * @Author jiyec
+ * @Date 2021/5/7 21:19
+ * @Version 1.0
+ **/
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebFilter(filterName = "AllFilter", urlPatterns = {
+        "*"
+})
+public class AllFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+}
