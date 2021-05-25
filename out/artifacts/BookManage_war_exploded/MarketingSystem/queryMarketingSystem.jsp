0d51ca839f1567fd057efb39f12826416820737a
Sorakado <kaga1i@outlook.com>
2021-05-25 17:33:24 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/webapp/MarketingSystem/queryMarketingSystem.jsp
    #	src/main/webapp/assets/css/MarketingSystem.css
    #	target/BookManage-1.0/MarketingSystem/queryMarketingSystem.jsp
    #	target/BookManage-1.0/WEB-INF/classes/cn/bookmanage/filter/AllFilter.class
    #	target/BookManage-1.0/assets/css/MarketingSystem.css
    #	target/classes/cn/bookmanage/filter/AllFilter.class

---
+++ out/artifacts/BookManage_war_exploded/MarketingSystem/queryMarketingSystem.jsp
@@ -0,0 +1,61 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 17237
+  Date: 2021/5/20
+  Time: 22:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page import="cn.bookmanage.service.impl.StoreServiceImpl" %>
+<%@ page import="cn.bookmanage.service.StoreService" %>
+<%@ page import="cn.bookmanage.entity.Book" %>
+<%@ page import="java.util.List" %>
+<%@ page import="java.util.Map" %>
+<%@ page import="cn.bookmanage.entity.User" %>
+<%@ page import="java.net.URLEncoder" %>
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page isELIgnored="false" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>This is Marketing System</title>
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/assets/css/MarketingSystem.css">
+    <script src="${pageContext.request.contextPath}/assets/js/MarketingSystem.js"></script>
+</head>
+<body>
+<h1>Welcome to MarketingSystem!</h1>
+
+<div class="box">
+    <div>查询</div>
+    <div>发布通知</div>
+    <div>查看通知</div>
+    <div>提交订书单</div>
+</div>
+<div class="box1">
+    <div class="item">
+        <ul>
+            <li>
+                <div>按书名查询</div>
+                <div>按作者查询</div>
+                <div>按ISBN查询</div>
+            </li>
+        </ul>
+    </div>
+    <div class="item">
+        请输入要提交的消息<input type="text">
+    </div>
+    <div class="item">消息栏</div>
+    <div class="item">
+        <select>
+            <option value="JSP实用教程（第4版）">JSP实用教程（第4版）</option>
+            <option value="计算机组成原理微课版">计算机组成原理微课版</option>
+            <option value="软件工程：实践者的研究方法（原书第8版）（本科教学版）">软件工程：实践者的研究方法（原书第8版）（本科教学版）</option>
+            <option value="web前端设计基础——html5、css3、java(二版)">web前端设计基础——html5、css3、java(二版)</option>
+            <option value="新目标大学英语系列教材：科技英语教程学生用书">新目标大学英语系列教材：科技英语教程学生用书</option>
+            <option value="概率论与数理统计及其应用（第2版）">概率论与数理统计及其应用（第2版）</option>
+        </select>数量：<input type="text">&nbsp;<input type="button" value="提交">
+    </div>
+</div>
+</body></body>
+</html>
---
+++ out/artifacts/BookManage_war_exploded/assets/js/common.js
@@ -0,0 +1,104 @@
+
+function httpGet(url) {
+    return new Promise((resolve, reject) => {
+        $.get(url, resolve);
+    });
+}
+function httpPost(url, param) {
+    return new Promise((resolve, reject) => {
+        $.post(url, param, res=>{
+            if(res.code && res.code === 2000){
+                resolve(res)
+            }else{
+                reject(res)
+            }
+        });
+    });
+}
+
+// 用户操作模块
+const USER = function (){
+    // 注销操作
+    const logout = function (){
+        httpGet("Api/User/logout").then(r => {
+            console.log(r)
+            if(r.code === 2000){
+                Swal.fire({
+                    title: '注销成功！',
+                    icon: 'success',
+                    html: '2秒后返回首页',
+                    showCloseButton: false,
+                    showCancelButton: false,
+                    focusConfirm: false
+                })
+                setTimeout(()=>{
+                    location.href = ""
+                }, 2000)
+            }
+        })
+    }
+
+    // 登录操作
+    const login = function (){
+        Swal.fire({
+            title: '请稍等...',
+            icon: 'info',
+            html:
+                '登录中...',
+            showCloseButton: false,
+            showCancelButton: false,
+            focusConfirm: false
+        })
+        // 发送 POST 请求
+        httpPost('LoginServlet', {
+            username: uinfo.username.value,
+            password: uinfo.password.value
+        })
+            .then(function (response) {
+                console.log(response);
+
+                const request = getRequest();
+                let redirection = request.redirect || "user/info.jsp"
+                Swal.fire({
+                    title: '登录成功！',
+                    icon: 'success',
+                    html: '准备跳转至' + redirection,
+                    showCloseButton: false,
+                    showCancelButton: false,
+                    focusConfirm: false
+                })
+
+                location.href = redirection;
+            })
+            .catch(function (error) {
+                console.log("错误", error);
+
+                Swal.fire({
+                    title: '登录失败！',
+                    icon: 'error',
+                    html: '<span style="color: red">' + error.msg + '</span>',
+                    showCloseButton: false,
+                    showCancelButton: false,
+                    focusConfirm: false,
+                    timer: 5000
+                })
+            });
+    }
+    return {
+        logout:logout,
+        login:login
+    }
+}();
+
+function getRequest() {
+    let url = location.search; //获取url中"?"符后的字串
+    let theRequest = new Object();
+    if (url.indexOf("?") != -1) {
+        let str = url.substr(1);
+        let strs = str.split("&");
+        for(let i = 0; i < strs.length; i ++) {
+            theRequest[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]);
+        }
+    }
+    return theRequest;
+}
